edbab2c3b3356e5c151559644f1aecdb00455dbe
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-26 10:44:24 +0300

    Add RequireLogin.js

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -25,7 +25,7 @@ const UsersList = () => {
   if (isSuccess) {
     const { ids } = users
 
-    const tableContent = ids?.length ? ids.map(userId => <User key={userId} userId={userId} />) : null
+    const tableContent = ids?.length && ids.map(userId => <User key={userId} userId={userId} />) 
 
     content = (
       <table className="table table--users">
